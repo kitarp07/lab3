0ddcc099ee4dead56753f04480811fc4456da2a7
kitarp07 <imlaleet@gmail.com>
2021-04-01 09:40:20 +0545

    temperature

--- a/temperature.py
+++ a/temperature.py
@@ -4,7 +4,7 @@ its a cold day; otherwise iits  neither cold nor hot
 '''
 
 temperature= float(input("enter the temperature: "))
-if  temperature>30:
+if  temperature > 30:
     print('its a hot day')
 elif temperature<10:
     print('its a cold day')
